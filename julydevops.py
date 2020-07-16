9413351dc85e2c030a36450b03f25254335338b8
K1610174 <kkekitinisa@gmail.com>
2020-07-16 17:27:47 +0100

    Challenge creating and using list of names.

---
+++ julydevops.py
@@ -0,0 +1,14 @@
+#initialises a list which contains the FIRST NAME of all 21 people in this cohort
+#using the .append() function add me to the list
+#print the 5th person in the list (this is a trick question, what is special about the way Python indexes?)
+#print the number of Chris in the cohort
+
+cohort = ["Amanda","Joshua","Chris","Chris","Tobias","John","Samuel","Jason","Mowafak","Sithembiso",
+          " Diarmuid","Edmund","Bradley","Steven","Jacob","Domenico","Ryan","Javas","Wasim","Clifford","Arsalan"]
+cohort.append("Luke")
+print(cohort[4])
+print(cohort.count("Chris"))
+
+
+
+#Once you have a solution, remember to push it to github. 

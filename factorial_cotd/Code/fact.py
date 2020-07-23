eb46e81a990a0afb1f51656ce5dc49cd010c860b
Amanda Kekitinisa <66065549+K1610174@users.noreply.github.com>
2020-07-23 18:11:19 +0100

    Update fact.py

--- factorial_cotd/Code/fact.py
+++ factorial_cotd/Code/fact.py
@@ -1,11 +1,11 @@
 #Write a function that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
 #Suppose the following input is supplied to the program: 9 Then, the output should be: 11106 (i.e. 9+99+999+9999)
 def fact(a,b):
-    str_a = str(a) 
-    sums= a
-    str_b = str_a   
+    stringa = str(a)  #convert value to string
+    sums= a           
+    stringb = stringa  
     for i in range(1,b):
-        str_a = str_a + str_b
-        sums = sums + int(str_a)
+        stringa = stringa + stringb  #making a+aa+aaa+aaaa (concatenating)
+        sums = sums + int(stringa)   #convert to integer to add
     return sums
 
